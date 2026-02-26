ab3cf0fae0634c02a10a71c19389e7dfb2ea3cc0
Jan Franciszek Adamski <adamski.jan.franciszek@gmail.com>
2026-02-26 13:50:20 +0100

    ✨ Added subpages for works, exh, about, contact

---
+++ src/app/about/page.tsx
@@ -0,0 +1,4 @@
+export default function About() {
+  return (
+  <>Lorem ipsum about</>);
+}
---
+++ src/app/exhibitions/page.tsx
@@ -0,0 +1,4 @@
+export default function Exhibitions() {
+  return (
+  <>Lorem ipsum exhibitions</>);
+}
